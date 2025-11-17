daeeb7b603014132d8ee74562d8702c178cb45ee
ndmichael <ukejemicheal@gmail.com>
2025-11-17 20:27:40 +0100

    layout improved

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,13 +1,16 @@
+import { Navbar } from '@/components/layout/Navbar';
+import { Footer } from '@/components/layout/Footer';
+
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      {/* Navbar will go here */}
-      <main className="flex-1">{children}</main>
-      {/* Footer will go here */}
-    </div>
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
   );
 }
