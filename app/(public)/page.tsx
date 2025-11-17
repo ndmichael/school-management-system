41ac036e34f2e15817bc29edfe454d6452e6f10b
ndmichael <ukejemicheal@gmail.com>
2025-11-17 20:28:14 +0100

    added an hero page

---
+++ app/(public)/page.tsx
@@ -0,0 +1,12 @@
+// app/(public)/page.tsx
+import { Hero } from '@/components/sections/Hero';
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      {/* Add more sections here later */}
+    </>
+  );
+}
+
