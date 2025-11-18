027d1bc3b2c328d19fd8593a9a7e0bd8341893a4
ndmichael <ukejemicheal@gmail.com>
2025-11-18 23:12:26 +0100

    return more data to the index page

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,5 +2,6 @@ export { PrimaryButton } from './PrimaryButton';
 export { SecondaryButton } from './SecondaryButton';
 export { SectionHeader } from './SectionHeader';
 export { ProgramCard } from './ProgramCard';
+export { ProgramModal } from './ProgramModal';
 export { FeatureCard } from './FeatureCard';
 export {TestimonialCard} from './TestimonialCard';
