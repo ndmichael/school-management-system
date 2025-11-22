43bac67660e57cef10142f82abf5844fd5889564
ndmichael <ukejemicheal@gmail.com>
2025-11-22 23:10:59 +0100

    allow images api

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,18 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
