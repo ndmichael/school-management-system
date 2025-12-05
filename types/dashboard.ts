10d9a630a48fa71bd4dfe962c80eea2fd2b8db10
ndmichael <ukejemicheal@gmail.com>
2025-12-05 08:55:44 +0100

    types for dashboard

---
+++ types/dashboard.ts
@@ -0,0 +1,9 @@
+// types/dashboard.ts
+export type UserRole = 'admin' | 'student' | 'academic_staff' | 'non_academic_staff'
+
+export interface DashboardUser {
+  id: string
+  fullName: string
+  email: string
+  role: UserRole
+}
