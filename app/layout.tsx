7b458493ce256b9de00ffcd7c14e544bb6b602c1
ndmichael <ukejemicheal@gmail.com>
2025-12-05 13:53:04 +0100

    Updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ 
   subsets: ["latin"],
@@ -24,6 +26,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         {children}
+        <ToastContainer position="top-right" autoClose={3000} />
       </body>
     </html>
   );
