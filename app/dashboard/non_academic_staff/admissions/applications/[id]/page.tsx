ac2ddab65a497a891e906273b95871872236a17f
ndmichael <ukejemicheal@gmail.com>
2026-03-07 12:31:34 +0100

    all updated

--- app/dashboard/non_academic_staff/admissions/applications/[id]/page.tsx
+++ app/dashboard/non_academic_staff/admissions/applications/[id]/page.tsx
@@ -1,1 +1,8 @@
-export { default } from "@/app/dashboard/admin/applications/[id]/page";
+// export { default } from "@/app/dashboard/admin/applications/[id]/page";
+import ApplicationDetailsPage from "@/components/applications/ApplicationDetailsPage"
+
+export default function AdmissionsApplicationDetailsRoute() {
+  return (
+    <ApplicationDetailsPage detailsBasePath="/dashboard/non_academic_staff/admissions/applications" />
+  )
+}
